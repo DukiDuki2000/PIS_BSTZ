40040ab498e44f4312dcb3b9a620372ebdbfc8a5
DukiDuki2000 <144245097+DukiDuki2000@users.noreply.github.com>
2024-05-11 23:24:45 +0200

    Add files via upload

---
+++ testowy/src/main/java/programtestowy/App.java
@@ -0,0 +1,13 @@
+package programtestowy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
