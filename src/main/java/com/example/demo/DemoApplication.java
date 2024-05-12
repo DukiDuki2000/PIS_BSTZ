7f4615f2d933ab0974e2b7f7843fa19caba094b2
DukiDuki2000 <144245097+DukiDuki2000@users.noreply.github.com>
2024-05-12 17:03:32 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,18 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+@Controller
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	@GetMapping
-	public String hello(){
-		return "Hello World";
 	}
 }
