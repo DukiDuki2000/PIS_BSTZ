925a67d2a2641b5bb58baa4a4d26c45032aed6c6
pskowron <01148804@pw.edu.pl>
2024-05-16 00:05:35 +0200

    Dodanie front+back

---
+++ src/main/java/com/example/konsultacje/KonsultacjeApplication.java
@@ -0,0 +1,13 @@
+package com.example.konsultacje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonsultacjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonsultacjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/konsultacje/KonsultacjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.konsultacje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonsultacjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
