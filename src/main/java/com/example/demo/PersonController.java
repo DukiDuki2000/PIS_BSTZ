423c26b692c7f9ba443191f19f0ec091cf0408e7
DukiDuki2000 <144245097+DukiDuki2000@users.noreply.github.com>
2024-05-21 19:32:57 +0200

    sql+sprng 3/3 bez 2/3

---
+++ src/main/java/com/example/demo/PersonController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.repo.PersonRepo;
+import com.example.demo.model.Person;
+@RestController
+public class PersonController {
+    @Autowired
+    PersonRepo repo;
+
+
+@PostMapping("/addPerson")
+    public void addPerson(@RequestBody Person person) {
+    repo.save(person);
+}
+
+}
