c119de9de7513b35d437375192ac32c5766181e0
pskowron <01148804@pw.edu.pl>
2024-05-24 23:39:45 +0200

    Dodanie sortowania po zapytaniach

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import axios from 'axios';
 
 function Books() {
     const [books, setBooks] = useState([]);
+    const [sortBy, setSortBy] = useState<string>(''); // Domyślnie pusty string
+    const [sortOrder, setSortOrder] = useState<string>('asc'); // Domyślnie 'asc'
 
     const fetchBooks = async () => {
         try {
-            const { data } = await axios.get('http://localhost:7788/books');
+            const params: { sortBy?: string, sortOrder?: string } = {};
+            if (sortBy) {
+                params.sortBy = sortBy;
+                params.sortOrder = sortOrder;
+            }
+            const { data } = await axios.get('http://localhost:7788/books', { params });
             setBooks(data);
         } catch (error) {
             console.error('There was an error fetching the books!', error);
@@ -15,10 +22,36 @@ function Books() {
 
     useEffect(() => {
         fetchBooks();
-    }, []);
+    }, [sortBy, sortOrder]);
+
+    const handleSortByChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSortBy(event.target.value);
+    };
+
+    const handleSortOrderChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setSortOrder(event.target.value);
+    };
 
     return (
         <div className='page_content'>
+            <div className='sort_controls'>
+                <label>
+                    Sortuj według:
+                    <select value={sortBy} onChange={handleSortByChange}>
+                        <option value='title'>Tytuł</option>
+                        <option value='authorName'>Imię autora</option>
+                        <option value='authorSurname'>Nazwisko autora</option>
+                        <option value='category'>Kategoria</option>
+                    </select>
+                </label>
+                <label>
+                    Kierunek sortowania:
+                    <select value={sortOrder} onChange={handleSortOrderChange}>
+                        <option value='asc'>Rosnąco</option>
+                        <option value='desc'>Malejąco</option>
+                    </select>
+                </label>
+            </div>
             <div className='books_grid'>
                 {books.map(({ id, title, authorName, authorSurname, category }) => (
                     <div key={id} className='book_tile'>
