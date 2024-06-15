32259605725fafa78ccbef482f3a606b3441651c
mbrus <michal.brus@dahliamatic.pl>
2024-06-15 23:39:55 +0200

    searchbar added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import * as PiIcons from 'react-icons/pi'
-import Sign_In_Button from './Sign_In_Button'
-import Books_Button from './Books_Button'
-import About_Us_Button from './About_Us_Button'
+import React from 'react';
+import * as PiIcons from 'react-icons/pi';
+import Sign_In_Button from './Sign_In_Button';
+import Books_Button from './Books_Button';
+import About_Us_Button from './About_Us_Button';
+import SearchBar from "./SearchBar";
+import '../App.css'; // Import the global CSS file
 
 const Header = () => {
-    return (
-        <header>
-            <nav className='header'>
-                <a href="/" id="logo">
-                    <PiIcons.PiBooks id="logo_books" size={40} />
-                    <span id="logo_text">Galaktyczne Lektury</span>
-                    <PiIcons.PiShootingStar id="logo_star" size={40} />
-                </a>
-                <Books_Button />
-                <About_Us_Button />
-                <Sign_In_Button />
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header">
+      <a href="/" id="logo">
+        <PiIcons.PiBooks id="logo_books" size={40} />
+        <span id="logo_text">Galaktyczne Lektury</span>
+        <PiIcons.PiShootingStar id="logo_star" size={40} />
+      </a>
+      <div className="nav-container">
+        <SearchBar />
+        <div className="nav-items">
+          <Books_Button />
+          <About_Us_Button />
+          <Sign_In_Button />
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
